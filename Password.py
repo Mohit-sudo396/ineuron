6b09eaa56d7885cbbea8df1d21a5c127afc6186a
Mohit-sudo396 <mvmvijay284@gmail.com>
2021-05-13 13:17:08 +0530

    passwordcommit

---
+++ Password.py
@@ -0,0 +1,6 @@
+
+User_name = input("Enter your username: ")
+Password = input("Enter your password: ")
+length = '*' * len(Password)
+
+print(f'{User_name}, your password is {length} letters long.')
